997aabdc3bfaa60ba6d482b55665e064e641e1d3
NicolasRAA <nicolas.arroyo1703@gmail.com>
2024-12-03 02:32:23 +0300

    I did the second task!

--- script.bash
+++ script.bash
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-x=$1
-y=$2
+if [ "$#" -ne 1 ]; then
+    echo "Uso: $0 <IPv4 dirección>"
+    exit 1
+fi
 
-sum=$(($x + $y))
-difference=$(($x - $y))
-product=$(($x * $y))
+ip=$1
 
-echo "$sum $difference $product"
+IFS='.' read -r oct1 oct2 oct3 oct4 <<< "$ip"
+
+bin_oct1=$(printf "%08d" "$(bc <<< "obase=2; $oct1")")
+bin_oct2=$(printf "%08d" "$(bc <<< "obase=2; $oct2")")
+bin_oct3=$(printf "%08d" "$(bc <<< "obase=2; $oct3")")
+bin_oct4=$(printf "%08d" "$(bc <<< "obase=2; $oct4")")
+
+echo "$bin_oct1.$bin_oct2.$bin_oct3.$bin_oct4"
